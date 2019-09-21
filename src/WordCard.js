f74c9e879f48356660af62a2471a8523d8b436d3
shinichiz1 <monngon191@hotmail.com>
2019-09-22 03:19:18 +0700

    improve round showing

--- src/WordCard.js
+++ src/WordCard.js
@@ -27,7 +27,7 @@ export default class Wordcard extends Component {
         this.setState({guess})
         if(guess.length == this.state.chars.length){
             if(guess.join('').toString() == this.state.word){
-                this.setState({guess: [], completed: true, isWin: true})
+                this.setState({guess: [], completed: true, isWin: true, attempt: this.state.attempt + 1})
             }else{
                 if(this.state.attempt == 3){
                     this.setState({guess: [], completed: true, isWin: false})
@@ -45,7 +45,7 @@ export default class Wordcard extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Round : {this.state.attempt} / 3</h1>
+                <h1>Round : {this.state.completed ? this.state.attempt - 1 : this.state.attempt} / 3</h1>
                 {Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} attempt = {this.state.attempt} activationHandler={this.activationHandler}/>)}
                 <p><button onClick={this.windowReload}>TryNewWord</button></p>
                 <p>{this.state.isWin ? <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoJQ3aO04SyaUV57ZsQ_eRz2bG8oyMpp28JVZTpjN1U0Iiz1ge" width = "300" height = "300"></img> : <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQH_-d2jrOIMM_nJMwkky0VkTvKtliFzBqGWSOaGG_KIrGylBVn" width = "300" height = "300"></img>}</p>
